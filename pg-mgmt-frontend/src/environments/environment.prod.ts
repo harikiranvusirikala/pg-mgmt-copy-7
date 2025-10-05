335751a2abe5f3539e4f1c0738dbb46576fddced
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2025-10-05 07:33:24 +0530

    Update production API base URL
    Changed the apiBaseUrl in the production environment configuration to use the new backend endpoint.

--- pg-mgmt-frontend/src/environments/environment.prod.ts
+++ pg-mgmt-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://saibabapg-test-backend.onrender.com',
+  apiBaseUrl: 'https://saibabapg-backend-test.onrender.com',
   googleClientId:
     '833966053041-2hb1gt2ieu80s42eajakbn6go2pilmuu.apps.googleusercontent.com',
 };
